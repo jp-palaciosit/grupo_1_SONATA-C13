9b92e1494e9a26a98da46703f756064a95948686
Juan Pablo P <juanchii10p@gmail.com>
2022-03-18 03:49:02 -0300

    Modularización del css

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const PORT = 3030
-const pathResolve = path.resolve(__dirname, "./public")
+const pathResolve = path.join(__dirname, "./public")
 
 app.use(express.static(pathResolve))
 
 app.listen(PORT, ()=>console.log(`Servidor corriendo en el puerto ${PORT}`))
 
 app.get("/", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+    res.sendFile(path.join(__dirname, "./views/index.html"))
 })
