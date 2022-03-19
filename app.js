2d71e3e918d7974713eb7ee6ec8e4c2f8022a21e
Juan Pablo P <juanchii10p@gmail.com>
2022-03-18 21:41:31 -0300

    se realizaron cambios

--- app.js
+++ app.js
@@ -11,3 +11,10 @@ app.listen(PORT, ()=>console.log(`Servidor corriendo en el puerto ${PORT}`))
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "./views/index.html"))
 })
+
+app.get("/productDetail", (req,res)=>{
+    res.sendFile(path.join(__dirname, "./views/productDetail.html"))
+})
+app.get("/productCart", (req,res)=>{
+    res.sendFile(path.join(__dirname, "./views/productCart.html"))
+})
