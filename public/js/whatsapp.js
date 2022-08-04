ecedfa8f803823dd2be71995f8c4c1f0c26d29a7
Kaiael24 <juanchii10p@gmail.com>
2022-08-04 07:32:31 -0300

    Creacion y edicion de categorias

---
+++ public/js/whatsapp.js
@@ -0,0 +1,9 @@
+let cartelWhatsapp = document.getElementById("cartelWhatsapp");
+
+ function clickWhatsapp() {
+  if (cartelWhatsapp.style.display === "block") {
+    cartelWhatsapp.style.display = "none";
+  } else {
+    cartelWhatsapp.style.display = "block";
+  }
+}
